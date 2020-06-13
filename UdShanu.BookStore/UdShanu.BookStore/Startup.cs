78007abf2e99ddb7e9d4941c091323357ab37ac1
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2020-06-14 00:03:37 +0530

    Setup DbContext class in entity framework core.

--- UdShanu.BookStore/UdShanu.BookStore/Startup.cs
+++ UdShanu.BookStore/UdShanu.BookStore/Startup.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using UdShanu.BookStore.Data;
 
 namespace UdShanu.BookStore
 {
@@ -18,6 +20,7 @@ namespace UdShanu.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookStoreContext>(options => options.UseMySQL("Server=localhost;UId=root;Pwd=admin;Database=BookStore"));
             services.AddControllersWithViews();
 #if DEBUG
             services.AddRazorPages().AddRazorRuntimeCompilation();
